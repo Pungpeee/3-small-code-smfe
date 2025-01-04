48a8ac63a6908fe4703998ebde2db76bde7d751a
Pungpeee <60164550+Pungpeee@users.noreply.github.com>
2025-01-04 13:36:35 +0700

    solve vul

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<!-- <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
-<!-- <template>
-  <div id="view">
-    <router-view/>
-  </div>
-</template> -->
-
 <template>
   <div id="nav" v-if="$store.state.user">
+    <!-- Uncomment below if navigation links are needed -->
     <!-- <router-link to="/">Home</router-link> -->
-    <!-- <router-link to="/todo">todo</router-link> -->
+    <!-- <router-link to="/todo">Todo</router-link> -->
     <!-- <button @click="$store.dispatch('logout')">Logout</button> -->
-  </div> 
-  <router-view/>
+  </div>
+  <router-view />
 </template>
 
 <script>
 import { onBeforeMount } from 'vue'
 import { useStore } from 'vuex'
+
 export default {
   setup() {
     const store = useStore()
@@ -51,6 +21,7 @@ export default {
   }
 }
 </script>
-<style >
 
+<style>
+/* Add custom styles here */
 </style>
